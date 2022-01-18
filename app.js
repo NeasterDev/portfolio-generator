6069ad4fff285f294f1a264be994005bc3f85d36
Nicholas Easter <nicholas.easter17@gmail.com>
2022-01-18 14:19:20 -0800

    added basic inquirer prompt

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+const { type } = require('os');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const promptUser = () => {
+  return inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+}
 
-const [firstName, github] = profileDataArgs;
+promptUser().then(answers => console.log(answers));
 
-fs.writeFile('./index.html', generatePage(firstName, github), err => {
-  if (err) throw err;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const pageHTML = generatePage(nome, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
